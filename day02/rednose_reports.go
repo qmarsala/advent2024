12803922af939b84f9347a957d0c9aba44416a67
Quinten Marsala <quinten.marsala@gmail.com>
2024-12-01 22:43:24 -0800

    part two

--- day02/rednose_reports.go
+++ day02/rednose_reports.go
@@ -13,8 +13,11 @@ func RunDay() {
 		fmt.Printf("Day02 - [ERROR]: %v\n", err)
 		return
 	}
-	part1 := CountSafeReports(reports)
-	part2 := 0
+	part1 := CountSafeReports(reports, func(r []int64) bool {
+		safe, _ := CheckPlantReport(r)
+		return safe
+	})
+	part2 := CountSafeReports(reports, CheckPlantReportWithDampening)
 	fmt.Printf("Day02: %v, %v \n", part1, part2)
 }
 
@@ -36,29 +39,38 @@ func ReadRedNosedReportInputs() (reports [][]int64, err error) {
 	return reports, nil
 }
 
-func CountSafeReports(reports [][]int64) (count int64) {
-	for _, v := range reports {
-		if CheckPlantReport(v) {
+func CountSafeReports(reports [][]int64, strategy func([]int64) bool) (count int64) {
+	for _, r := range reports {
+		if safe := strategy(r); safe {
 			count += 1
 		}
 	}
 	return
 }
 
-func CheckPlantReport(report []int64) bool {
+func CheckPlantReport(report []int64) (safe bool, problemIndex int) {
 	current_level := report[0]
 	is_desc := current_level > report[1]
 	for i := 1; i < len(report); i++ {
 		diff := GetDiff(current_level, report[i])
 		if diff < 1 || diff > 3 {
-			return false
+			return false, i - 1
 		}
 		if (is_desc && current_level < report[i]) || (!is_desc && current_level > report[i]) {
-			return false
+			return false, i - 1
 		}
 		current_level = report[i]
 	}
-	return true
+	return true, -1
+}
+
+func CheckPlantReportWithDampening(report []int64) (safe bool) {
+	safe, problemIndex := CheckPlantReport(report)
+	if !safe {
+		new_report := RemoveIndex(report, problemIndex)
+		safe, _ = CheckPlantReport(new_report)
+	}
+	return
 }
 
 func GetDiff(a int64, b int64) int64 {
@@ -67,4 +79,10 @@ func GetDiff(a int64, b int64) int64 {
 	} else {
 		return a - b
 	}
+}
+
+func RemoveIndex(s []int64, index int) []int64 {
+	ret := make([]int64, 0)
+	ret = append(ret, s[:index]...)
+	return append(ret, s[index+1:]...)
 }
--- day02/rednose_reports_test.go
+++ day02/rednose_reports_test.go
@@ -8,7 +8,7 @@ import (
 func TestRedNosedReports(t *testing.T) {
 	t.Run("7 6 4 2 1 = safe", func(t *testing.T) {
 		report := []int64{7, 6, 4, 2, 1}
-		result := day02.CheckPlantReport(report)
+		result, _ := day02.CheckPlantReport(report)
 
 		if !result {
 			t.Errorf("expected safe, but was unsafe")
@@ -16,7 +16,7 @@ func TestRedNosedReports(t *testing.T) {
 	})
 	t.Run("1 2 7 8 9 = unsafe", func(t *testing.T) {
 		report := []int64{1, 2, 7, 8, 9}
-		result := day02.CheckPlantReport(report)
+		result, _ := day02.CheckPlantReport(report)
 
 		if result {
 			t.Errorf("expected unsafe, but was safe")
@@ -24,7 +24,7 @@ func TestRedNosedReports(t *testing.T) {
 	})
 	t.Run("9 7 6 2 1 = unsafe", func(t *testing.T) {
 		report := []int64{9, 7, 6, 2, 1}
-		result := day02.CheckPlantReport(report)
+		result, _ := day02.CheckPlantReport(report)
 
 		if result {
 			t.Errorf("expected unsafe, but was safe")
@@ -32,23 +32,39 @@ func TestRedNosedReports(t *testing.T) {
 	})
 	t.Run("1 3 2 4 5 = unsafe", func(t *testing.T) {
 		report := []int64{1, 3, 2, 4, 5}
-		result := day02.CheckPlantReport(report)
+		result, _ := day02.CheckPlantReport(report)
 
 		if result {
 			t.Errorf("expected unsafe, but was safe")
+		}
+	})
+	t.Run("1 3 2 4 5 = safe with dampener", func(t *testing.T) {
+		report := []int64{1, 3, 2, 4, 5}
+		result := day02.CheckPlantReportWithDampening(report)
+
+		if !result {
+			t.Errorf("expected safe, but was unsafe")
 		}
 	})
 	t.Run("8 6 4 4 1 = unsafe", func(t *testing.T) {
 		report := []int64{8, 6, 4, 4, 1}
-		result := day02.CheckPlantReport(report)
+		result, _ := day02.CheckPlantReport(report)
 
 		if result {
 			t.Errorf("expected unsafe, but was safe")
 		}
 	})
+	t.Run("8 6 4 4 1 = safe with dampener", func(t *testing.T) {
+		report := []int64{8, 6, 4, 4, 1}
+		result := day02.CheckPlantReportWithDampening(report)
+
+		if !result {
+			t.Errorf("expected safe, but was unsafe")
+		}
+	})
 	t.Run("1 3 6 7 9 = safe", func(t *testing.T) {
 		report := []int64{1, 3, 6, 7, 9}
-		result := day02.CheckPlantReport(report)
+		result, _ := day02.CheckPlantReport(report)
 
 		if !result {
 			t.Errorf("expected safe, but was unsafe")
@@ -59,10 +75,17 @@ func TestRedNosedReports(t *testing.T) {
 		report2 := []int64{1, 3, 6, 7, 9}
 		report3 := []int64{1, 3, 6, 7, 9}
 		reports := [][]int64{report1, report2, report3}
-		result := day02.CountSafeReports(reports)
+		result1 := day02.CountSafeReports(reports, func(r []int64) bool {
+			safe, _ := day02.CheckPlantReport(r)
+			return safe
+		})
+		result2 := day02.CountSafeReports(reports, day02.CheckPlantReportWithDampening)
 
-		if result != 3 {
-			t.Errorf("expected 3, but got %v", result)
+		if result1 != 3 {
+			t.Errorf("expected 3, but got %v", result1)
+		}
+		if result2 != 3 {
+			t.Errorf("expected 3, but got %v", result2)
 		}
 	})
 }
