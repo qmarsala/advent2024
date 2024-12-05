55ff28403bbf0e33459d102af040a0ba51c37f40
Quinten Marsala <quinten.marsala@gmail.com>
2024-12-04 18:23:49 -0800

    day 4 part 1 (almost)

--- day04/word_search.go
+++ day04/word_search.go
@@ -1,18 +1,85 @@
 package day04
 
-import "fmt"
+import (
+	"advent2024/fileio"
+	"fmt"
+	"strings"
+)
 
 func RunDay() {
-	_, err := ReadWordSearchInputs()
+	input, err := ReadWordSearchInputs()
 	if err != nil {
 		fmt.Printf("Day04 - [ERROR]: %v\n", err)
 		return
 	}
-	part1 := 0
+	part1 := CountXMAS(input)
 	part2 := 0
 	fmt.Printf("Day04: %v, %v \n", part1, part2)
 }
 
 func ReadWordSearchInputs() (input [][]rune, err error) {
-	return make([][]rune, 0), nil
+	lines, err := fileio.ReadAllLines("./day04/input.txt")
+	if err != nil {
+		return make([][]rune, 0), nil
+	}
+	for _, l := range lines {
+		input = append(input, []rune(l))
+	}
+	return input, nil
+}
+
+const (
+	horizontal = iota
+	vertical
+	ascending
+	descending
+)
+
+func CountXMAS(input [][]rune) int {
+	xmasCount := 0
+	for row := 0; row < len(input); row++ {
+		for column := 0; column < len(input[row]); column++ {
+			if strings.ToLower(string(input[row][column])) == "x" || strings.ToLower(string(input[row][column])) == "s" {
+				if ReadXMAS(input, row, column, horizontal) ||
+					ReadXMAS(input, row, column, vertical) ||
+					ReadXMAS(input, row, column, ascending) ||
+					ReadXMAS(input, row, column, descending) {
+					xmasCount += 1
+				}
+			}
+		}
+	}
+	return xmasCount
+}
+
+func ReadXMAS(input [][]rune, startRow int, startColumn int, direction int) bool {
+	word := ""
+	for i := 0; i < 4; i++ {
+		switch direction {
+		case horizontal:
+			if startRow >= len(input) || startColumn+i >= len(input[startRow]) {
+				return false
+			}
+			word += string(input[startRow][startColumn+i])
+		case vertical:
+			if startRow+i >= len(input) || startColumn >= len(input[startRow]) {
+				return false
+			}
+			word += string(input[startRow+i][startColumn])
+		case ascending:
+			if startRow+i >= len(input) || startColumn+i >= len(input[startRow]) {
+				return false
+			}
+			word += string(input[startRow+i][startColumn+i])
+		case descending:
+			if startRow+i >= len(input) || startColumn-i < 0 {
+				return false
+			}
+			word += string(input[startRow+i][startColumn-i])
+		default:
+			return false
+		}
+	}
+	lowerWord := strings.ToLower(string(word))
+	return lowerWord == "xmas" || lowerWord == "samx"
 }
--- day04/word_search_test.go
+++ day04/word_search_test.go
@@ -1,1 +1,147 @@
 package day04_test
+
+import (
+	"advent2024/day04"
+	"testing"
+)
+
+// can I convert 2d to 1d? the offset my search by 'row length'?
+
+func TestCorruptedMemory(t *testing.T) {
+	t.Run("xmas", func(t *testing.T) {
+		input := [][]rune{
+			{'x', 'm', 'a', 's'},
+		}
+		result := day04.CountXMAS(input)
+		if result != 1 {
+			t.Errorf("expected 1, but got %v", result)
+		}
+	})
+
+	t.Run("samx", func(t *testing.T) {
+		input := [][]rune{
+			{'s', 'a', 'm', 'x'},
+		}
+		result := day04.CountXMAS(input)
+		if result != 1 {
+			t.Errorf("expected 1, but got %v", result)
+		}
+	})
+
+	t.Run("xmas diagonal descending", func(t *testing.T) {
+		input := [][]rune{
+			{'x', '.', '.', '.'},
+			{'.', 'm', '.', '.'},
+			{'.', '.', 'a', '.'},
+			{'.', '.', '.', 's'},
+		}
+		result := day04.CountXMAS(input)
+		if result != 1 {
+			t.Errorf("expected 1, but got %v", result)
+		}
+	})
+
+	t.Run("samx diagonal descending", func(t *testing.T) {
+		input := [][]rune{
+			{'s', '.', '.', '.'},
+			{'.', 'a', '.', '.'},
+			{'.', '.', 'm', '.'},
+			{'.', '.', '.', 'x'},
+		}
+		result := day04.CountXMAS(input)
+		if result != 1 {
+			t.Errorf("expected 1, but got %v", result)
+		}
+	})
+
+	t.Run("samx diagonal ascending", func(t *testing.T) {
+		input := [][]rune{
+			{'.', '.', '.', 'x'},
+			{'.', '.', 'm', '.'},
+			{'.', 'a', '.', '.'},
+			{'s', '.', '.', '.'},
+		}
+		result := day04.CountXMAS(input)
+		if result != 1 {
+			t.Errorf("expected 1, but got %v", result)
+		}
+	})
+
+	t.Run("xmas diagonal ascending", func(t *testing.T) {
+		input := [][]rune{
+			{'.', '.', '.', 's'},
+			{'.', '.', 'a', '.'},
+			{'.', 'm', '.', '.'},
+			{'x', '.', '.', '.'},
+		}
+		result := day04.CountXMAS(input)
+		if result != 1 {
+			t.Errorf("expected 1, but got %v", result)
+		}
+	})
+
+	t.Run("xmas vertical", func(t *testing.T) {
+		input := [][]rune{
+			{'x', '.', '.', '.'},
+			{'m', '.', '.', '.'},
+			{'a', '.', '.', '.'},
+			{'s', '.', '.', '.'},
+		}
+		result := day04.CountXMAS(input)
+		if result != 1 {
+			t.Errorf("expected 1, but got %v", result)
+		}
+	})
+
+	t.Run("samx vertical", func(t *testing.T) {
+		input := [][]rune{
+			{'s', '.', '.', '.'},
+			{'a', '.', '.', '.'},
+			{'m', '.', '.', '.'},
+			{'x', '.', '.', '.'},
+		}
+		result := day04.CountXMAS(input)
+		if result != 1 {
+			t.Errorf("expected 1, but got %v", result)
+		}
+	})
+
+	t.Run("xmas example", func(t *testing.T) {
+		input := [][]rune{
+			{'M', 'M', 'M', 'S', 'X', 'X', 'M', 'A', 'S', 'M'},
+			{'M', 'S', 'A', 'M', 'X', 'M', 'S', 'M', 'S', 'A'},
+			{'A', 'M', 'X', 'S', 'X', 'M', 'A', 'A', 'M', 'M'},
+			{'M', 'S', 'A', 'M', 'A', 'S', 'M', 'S', 'M', 'X'},
+			{'X', 'M', 'A', 'S', 'A', 'M', 'X', 'A', 'M', 'M'},
+			{'X', 'X', 'A', 'M', 'M', 'X', 'X', 'A', 'M', 'A'},
+			{'S', 'M', 'S', 'M', 'S', 'A', 'S', 'X', 'S', 'S'},
+			{'S', 'A', 'X', 'A', 'M', 'A', 'S', 'A', 'A', 'A'},
+			{'M', 'A', 'M', 'M', 'M', 'X', 'M', 'M', 'M', 'M'},
+			{'M', 'X', 'M', 'X', 'A', 'X', 'M', 'A', 'S', 'X'},
+		}
+		result := day04.CountXMAS(input)
+		if result != 18 {
+			t.Errorf("expected 18, but got %v", result)
+		}
+	})
+
+	t.Run("xmas example with .", func(t *testing.T) {
+		input := [][]rune{
+			{'.', '.', '.', '.', 'X', 'X', 'M', 'A', 'S', '.'},
+			{'.', 'S', 'A', 'M', 'X', 'M', 'S', '.', '.', '.'},
+			{'.', '.', '.', 'S', '.', '.', 'A', '.', '.', '.'},
+			{'.', '.', 'A', '.', 'A', '.', 'M', 'S', '.', 'X'},
+			{'X', 'M', 'A', 'S', 'A', 'M', 'X', '.', 'M', 'M'},
+			{'X', '.', '.', '.', '.', '.', 'X', 'A', '.', 'A'},
+			{'S', '.', 'S', '.', 'S', '.', 'S', '.', 'S', 'S'},
+			{'.', 'A', '.', 'A', '.', 'A', '.', 'A', '.', 'A'},
+			{'.', '.', 'M', '.', 'M', '.', 'M', '.', 'M', 'M'},
+			{'.', 'X', '.', 'X', '.', 'X', 'M', 'A', 'S', 'X'},
+		}
+		result := day04.CountXMAS(input)
+		if result != 18 {
+			t.Errorf("expected 18, but got %v", result)
+		}
+	})
+
+}
