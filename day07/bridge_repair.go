225f7df667d137e0fe682050256dfee8971268df
Quinten Marsala <quinten.marsala@gmail.com>
2024-12-07 13:17:15 -0800

    add example test case

--- day07/bridge_repair.go
+++ day07/bridge_repair.go
@@ -5,3 +5,7 @@ import "fmt"
 func RunDay(name string) {
 	fmt.Printf("%v wip\n", name)
 }
+
+func SumSolvableEquations(input map[int64][]int64) int64 {
+	return 0
+}
--- day07/bridge_repair_test.go
+++ day07/bridge_repair_test.go
@@ -1,12 +1,24 @@
 package day07_test
 
 import (
+	"advent2024/day07"
 	"testing"
 )
 
 func TestBridgeRepair(t *testing.T) {
 	t.Run("example", func(t *testing.T) {
-		result := 0
+		input := map[int64][]int64{
+			190:    {10, 19},
+			3267:   {81, 40, 27},
+			83:     {17, 5},
+			156:    {15, 6},
+			7290:   {6, 8, 6, 15},
+			161011: {16, 10, 13},
+			192:    {17, 8, 14},
+			21037:  {9, 7, 18, 13},
+			292:    {11, 6, 16, 20},
+		}
+		result := day07.SumSolvableEquations(input)
 		if result != 1 {
 			t.Errorf("expected 1, but got %v", result)
 		}
